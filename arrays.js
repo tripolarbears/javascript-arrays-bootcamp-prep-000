ef944acd70079d3907072c19849046c92f300a17
fearless-snippet-5362 <cpk1115@gmail.com>
2019-03-23 19:01:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
   var array = ["blah", "the", "boop"]
-  array.unshift(n)
+  array.unshift(element)
 }
