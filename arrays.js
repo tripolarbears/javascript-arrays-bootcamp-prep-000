a90f41e7068161187e49e97b69f3660d0760b03f
fearless-snippet-5362 <cpk1115@gmail.com>
2019-03-23 19:00:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"]
 
-function addElementToBeginningOfArray(n)
+function addElementToBeginningOfArray(n, foo) {
+  var array = ["blah", "the", "boop"]
+  array.unshift(n)
+}
